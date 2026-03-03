a06399eca45b65bbd024a67af6a742303ab4b9e1
bbm <brian.maranville@nist.gov>
2026-03-03 11:29:39 -0500

    generate scattering_table.html and deploy in same folder as activation calculator

--- deploy_calculator.sh
+++ deploy_calculator.sh
@@ -17,8 +17,11 @@ cp activation/resonance.html $TARGET_DIR/
 cp -r activation/pyodide $TARGET_DIR/pyodide
 cp -r activation/css $TARGET_DIR/
 
+# generate the HTML table and write it to the target folder
+python3 util/scattering_table_html.py $TARGET_DIR
+
 # Get the version of periodictable from the wheel file name and write it to a text file for use in the workflow
-PERIODICTABLE_VERSION=$(python -c "import periodictable; print(periodictable.__version__)")
+PERIODICTABLE_VERSION=$(python3 -c "import periodictable; print(periodictable.__version__)")
 
 # Write replacements in template
 API_SUB="s@{{ api_script }}@api_webworker.js@g"
